68e20d88b8f27734a9e52a2d678a07bf48804d45
Rezaul Hassan <RezhaBlue@users.noreply.github.com>
2016-12-03 04:29:30 -0500

    Updated Menu UI, minor overhaul of Bag, added makeUL utility function
    the menu js has been cleaned up and named a little smarter
    
    the html bag module is now populated using the makeUL function for each specific category's list
    the lists needs some ui work
    item traversal is broken for the moment in an effort to rework bag category traversal
    may need to change display structure in html

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
-var hNode;
+var textNode;
 var m;
 ////////////////ITEM_CLASS////////////////
 
 ////////////////MENU_MODULE////////////////
-var dmmenu = document.getElementById("menu");
+var dmmenu = document.getElementById("dm-menu");
 //med, item, tm, berries, key items, z
 var menu = ["Pokedex","Pokemon", 
             "Bag", "Options"];
 var menuCategories = {"Pokedex":0, "Pokemon":1, "Bag":2, "Options":3}
 //Populate Menu list
 //TODO: create whole LI inside for loop
-var mo = dmmenu.getElementsByClassName("menuOpt");
-for (m = 0; m < mo.length; m++) {
-    hNode = document.createTextNode(menu[m]);
-    mo[m].appendChild(hNode);
-    mo[m].tabIndex=1;
+var menuOptions = dmmenu.getElementsByClassName("menuOpt");
+for (m = 0; m < menuOptions.length; m++) {
+    textNode = document.createTextNode(menu[m]);
+    menuOptions[m].appendChild(textNode);
+    menuOptions[m].tabIndex=1;
 }
 //Give default focus to Pokedex
-mo[menuCategories.Pokedex].focus();
+menuOptions[menuCategories.Pokedex].focus();
 /////////////////////////////////////////////////Menu Keyboard Controls Event Listener & Handler
 dmmenu.addEventListener('keydown', function kd(event) {
     //Press the Left direction key
     if(event.keyCode == 37) {
-      mo[(m+mo.length-1) % mo.length].focus();
-      m = (m+mo.length-1) % mo.length;
+      menuOptions[(m+menuOptions.length-1) % menuOptions.length].focus();
+      m = (m+menuOptions.length-1) % menuOptions.length;
     }
     //Press the Right direction key
     else if(event.keyCode == 39) {
-      mo[(m+mo.length+1) % mo.length].focus();
-      m = (m+mo.length+1) % mo.length;
+      menuOptions[(m+menuOptions.length+1) % menuOptions.length].focus();
+      m = (m+menuOptions.length+1) % menuOptions.length;
     }
     //Press the Up direction key
     else if(event.keyCode == 38) {
-      mo[(m+mo.length-1) % mo.length].focus();
-      m = (m+mo.length-1) % mo.length;
+      menuOptions[(m+menuOptions.length-1) % menuOptions.length].focus();
+      m = (m+menuOptions.length-1) % menuOptions.length;
     }
     //Press the Down direction key
     else if(event.keyCode == 40) {
-      mo[(m+mo.length+1) % mo.length].focus();
-      m = (m+mo.length+1) % mo.length;     
+      menuOptions[(m+menuOptions.length+1) % menuOptions.length].focus();
+      m = (m+menuOptions.length+1) % menuOptions.length;     
     }
     //Press the Z (action) key
     else if(event.keyCode == 90) {
       dmmenu.style.display = "none";
-      dmbag.style.display = "block";
-      dmbag.focus();
+  //NOTE: hardcoded to go to bag
+      //TODO: make generic
+      dmbag.style.display = "inline-block";
+      bagOptions[bagCategories.Medicine].focus();
     }
     //Press the X (action) key
     else if(event.keyCode == 88) {
@@ -55,19 +57,7 @@ dmmenu.addEventListener('keydown', function kd(event) {
 });
 ////////////////BAG_MODULE////////////////
 //TODO: add, remove, & sort items functions
-var dmbag = document.getElementById("dm-bag")
-var i = 0;
-var j = 0;
-var pNode;
-
-/* hash setup 
-var bag = {
-    "berries": ["mago","chesto", "razz"], 
-    "items": ["pokeball", "mach belt"],
-    "free space": ["nothing"]
-};
-document.getElementById("demo").innerHTML =
-bag.berries + "<br>" + bag.items[2]; */
+var dmbag = document.getElementById("dm-bag");
 //med, item, tm, berries, key items, z
 var bag = [
   ["Potion","Hyper potion","Revival herb"],
@@ -77,66 +67,74 @@ var bag = [
   ["Old Rod", "Acro Bike", "VS Recorder"],
   ["nothing"]
 ];
-var bagCategories = {"Medicine":0, "Items":1, "TM":2, "Berries":3,"Key Items":4, "Free Space":5}
+var bagCategories = {"Medicine":0, "Items":1, "TM":2, "Berries":3,"KeyItems":4, "FreeSpace":5}
 
-//Display list (Medicine) as default
-pNode = document.createElement('div');
-pNode.innerHTML = bag[(i+bag.length) % bag.length] + "<br>";
-      //rmv class itmframe when looking at list
-      pNode.className = pNode.className.replace(/(?:^|\s)itmframe(?!\S)/g , '' );
-      dmbag.appendChild(pNode);
+var bagOptions = dmbag.getElementsByClassName("bagOpt");
+//Create the medicine html list
+bagOptions[bagCategories.Medicine].appendChild(makeUL(bag[bagCategories.Medicine],"medOpt"));
+//Create the item html list
+bagOptions[bagCategories.Items].appendChild(makeUL(bag[bagCategories.Items],"itemOpt"));
+//Create the tm html list
+bagOptions[bagCategories.TM].appendChild(makeUL(bag[bagCategories.TM],"tmOpt"));
+//Create the berries html list
+bagOptions[bagCategories.Berries].appendChild(makeUL(bag[bagCategories.Berries],"berOpt"));
+//Create the key items html list
+bagOptions[bagCategories.KeyItems].appendChild(makeUL(bag[bagCategories.KeyItems],"keyOpt"));
+//Create the free space html list
+bagOptions[bagCategories.FreeSpace].appendChild(makeUL(bag[bagCategories.FreeSpace],"fspOpt"));
 
-////////////////////////////////////////////////
-//Bag Keyboard Controls Event Listener & Handler
-//
+/////////////////////////////////////////////////Bag Category Keyboard Controls Event Listener & Handler
 dmbag.addEventListener('keydown', function kd(event) {
-/*limits list to linear traversal, not circular
-    if ((i % bag.length) === 0) { 
-      i = 0
-    } */
-//Press the Left direction key
+    //Press the Left direction key
     if(event.keyCode == 37) {
-      pNode.innerHTML = bag[(i+bag.length-1) % bag.length] + "<br>";
-      //rmv class itmframe when looking at list
-      pNode.className = pNode.className.replace(/(?:^|\s)itmframe(?!\S)/g , '' );
-      dmbag.appendChild(pNode);      
-      i = (i+bag.length-1) % bag.length;
+      bagOptions[(m+bagOptions.length-1) % bagOptions.length].focus();
+      m = (m+bagOptions.length-1) % bagOptions.length;
     }
-//Press the Right direction key
+    //Press the Right direction key
     else if(event.keyCode == 39) {
-      pNode.innerHTML = bag[(i+bag.length+1) % bag.length] + "<br>";
-      //rmv class itmframe when looking at list
-      pNode.className = pNode.className.replace(/(?:^|\s)itmframe(?!\S)/g , '' );      
-      dmbag.appendChild(pNode);      
-      i = (i+1) % bag.length;
+      bagOptions[(m+bagOptions.length+1) % bagOptions.length].focus();
+      m = (m+bagOptions.length+1) % bagOptions.length;
     }
-//Press the Up direction key
+    //Press the Up direction key
     else if(event.keyCode == 38) {
-      pNode.innerHTML = bag[(i+bag.length) % bag.length][(j+bag[(i+bag.length) % bag.length].length-1) % bag[(i+bag.length) % bag.length].length] + "<br>";
-      //add class itmframe when looking at item
-      pNode.className = "itmframe";
-//      dmbag.appendChild(pNode);
-      j = (j-1) % bag[(i+bag.length) % bag.length].length
-   }
-//Press the Down direction key
+
+    }
+    //Press the Down direction key
     else if(event.keyCode == 40) {
-      pNode.innerHTML = bag[(i+bag.length) % bag.length][(j+bag[(i+bag.length) % bag.length].length+1) % bag[(i+bag.length) % bag.length].length] + "<br>";
-      //add class itmframe when looking at item 
-      pNode.className = "itmframe";
-//      dmbag.appendChild(pNode);
-      j = (j+1) % bag[(i+bag.length) % bag.length].length
-   }
-  //Press the Z (action) key
+
+    }
+    //Press the Z (action) key
     else if(event.keyCode == 90) {
-      
-   }
-  //Press the X (action) key
+    }
+    //Press the X (action) key
     else if(event.keyCode == 88) {
-      pNode.innerHTML = bag[(i+bag.length) % bag.length][(j+bag[(i+bag.length) % bag.length].length+1) % bag[(i+bag.length) % bag.length].length] + "<br>";
-      //add class itmframe when looking at item 
-      pNode.className = "itmframe";
-//      dmbag.appendChild(pNode);
-      j = (j+1) % bag[(i+bag.length) % bag.length].length
-   }
+      dmbag.style.display = "none";
+      dmmenu.style.display = "inline-block";
+      menuOptions[menuCategories.Bag].focus();
+    }
   event.stopPropagation();
 });
+
+
+//===========================================//
+////////////////UTILITY_LIBRARY////////////////
+/*----Turns array into HTML ul----*/
+function makeUL(array, clsName) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+        // Set its class name:  
+        item.className = clsName;
+        // Set its contents:
+        var tNode = document.createTextNode(array[i]);
+        item.appendChild(tNode);
+        item.tabIndex = 1;
+        // Add it to the list:
+        list.appendChild(item);
+    }
+    // Finally, return the constructed list:
+    return list;
+}
